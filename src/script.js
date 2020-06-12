8d67a767797c6f6a4cf91376287bf82566602af4
RepleDEV <ragazzochaesa@gmail.com>
2020-06-12 08:57:49 +0700

    commit 6/12

--- src/script.js
+++ src/script.js
@@ -1,9 +1,16 @@
 const menu_start = document.getElementsByClassName("menu-start");
 const menu_main  = document.getElementsByClassName("menu-main");
 
+const timer = document.getElementById("timer");
+
 const outbox = document.getElementById("outbox");
+const inbox = document.getElementById("inbox");
 
 const genRanWords = n => randomWords(n);
+var generatedWords = genRanWords(120);
+const updateWords = () => outbox.innerHTML = `${generatedWords.join(" ")}...`;
+
+const updateTimer = time => timer.innerHTML = time;
 
 function buttonClick(e) {
     switch (e){
@@ -20,12 +27,15 @@ function buttonClick(e) {
     }
 }
 
+var startTestTog = 0;
+
 function startTest(time) {
-    var generatedWords = genRanWords(120);
-    const updateWords = () => outbox.innerHTML = `${generatedWords.join(" ")}...`;
     updateWords();
     //That's the setup. Next is the TRUE mechanics ;)
-    test_normal(time);
+    inbox.oninput = () => {
+        if (startTestTog == 0)test_normal(time);
+        startTestTog = 1;
+    }   
 }
 
 function test_normal(time) {
@@ -34,6 +44,7 @@ function test_normal(time) {
     loop();
 
     function loop() {
+        updateTimer(timer);
         if (timer <= 0)return;
         console.log(timer);
         setTimeout(() => {
